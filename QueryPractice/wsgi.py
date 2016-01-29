ab2e1f38ea6dad96ff164eccd7f10558319336e9
Myles <mcooney9790@gmail.com>
2016-01-28 19:49:45 -0600

    fixing everything

--- QueryPractice/wsgi.py
+++ QueryPractice/wsgi.py
@@ -8,11 +8,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "QueryPractice.settings")
 
+from dj_static import Cling
+#from whitenoise.django import DjangoWhiteNoise
 
 application = Cling(get_wsgi_application())
+#application = DjangoWhiteNoise(application)
