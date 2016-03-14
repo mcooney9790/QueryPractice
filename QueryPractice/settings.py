0d7abca4420ac04e486dbdafe6e252f119636c6d
Myles <mcooney9790@gmail.com>
2016-03-14 06:16:57 -0500

    bug fix 3

--- QueryPractice/settings.py
+++ QueryPractice/settings.py
@@ -119,17 +119,18 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'QueryStaticRoot/')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'QueryStaticRoot')
 
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
