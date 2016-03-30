27f8875d6e870b5534a6c61b5433e823dfeb5569
Myles <mcooney9790@gmail.com>
2016-03-30 18:29:15 -0500

    Add Rappers data  3/30/16

--- querytool/models.py
+++ querytool/models.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+# ======================
+#   Cities and Comedians Tables
+# =======================
 
 class Cities(models.Model):
 
@@ -29,6 +31,43 @@ class Club(models.Model):
 
     def __unicode__(self):
         return self.name
+
+# ======================
+#   Rappers
+# =======================
+
+class Rapper(models.Model):
+    name = models.CharField(max_length=100)
+    records_dropped = models.IntegerField()
+    rap_battles_won = models.IntegerField()
+    money_spent_on_strippers_2014 = models.IntegerField()
+    money_spent_on_strippers_2015 = models.IntegerField()
+    gold_chains_owned = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+    rapper = models.CharField(max_length=100)
+    copies_sold_first_week = models.IntegerField()
+    quality_score = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+class NightClub(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    top_spender = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+# ======================
+#   Questions
+# =======================
 
 class LessonQuestions(models.Model):
 
