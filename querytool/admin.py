879756bcb3cacb55e6179afcdfe6939e117ee221
Myles <mcooney9790@gmail.com>
2016-03-31 18:58:27 -0500

    3/31 register models

--- querytool/admin.py
+++ querytool/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Cities, Comedians, LessonQuestions
+from .models import Cities, Comedians, LessonQuestions, Club, Rapper, Album, NightClub
 # Register your models here.
 
 
 admin.site.register(Cities)
 admin.site.register(Comedians)
+admin.site.register(Club)
+admin.site.register(Rapper)
+admin.site.register(Album)
+admin.site.register(NightClub)
 admin.site.register(LessonQuestions)
 
 
